484ac31aa51f1a46b777224bfdd1d05005a03a43
SebaChuaqui <schuaquicano@gmail.com>
2021-07-23 20:54:19 -0400

    Ejercicios Consultas SQL III Parte 4

--- flim.sql
+++ flim.sql
@@ -18,6 +18,25 @@ WHERE category.name = 'Comedy'
 ORDER BY film.film_id ASC;
 
 --3--
+SELECT film_actor.actor_id AS 'Actor ID', CONCAT_WS(" ", actor.first_name, actor.last_name) AS 'Nombre', 
+film.title AS 'Titulo', film.description AS 'Descripcion', 
+film.release_year AS 'Estreno' FROM actor
+JOIN film_actor ON actor.actor_id = film_actor.actor_id
+JOIN film ON film_actor.film_id = film.film_id
+WHERE actor.actor_id = 5;
+
+--4--
+
+SELECT customer.store_id AS 'Tienda', address.city_id AS 'Ciudad', 
+CONCAT_WS(" ", customer.first_name, customer.last_name) AS 'Nombre', customer.email AS 'Correo',
+address.address AS 'Direccion'FROM customer
+JOIN address ON customer.address_id = address.address_id
+WHERE customer.store_id = 1 AND address.city_id IN (1, 42,312,459);
+
+
+
+
+
 
 
 
