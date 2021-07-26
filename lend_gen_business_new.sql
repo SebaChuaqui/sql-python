787f72662d3895965cb6c9c1f46bbcac3d78f3eb
SebaChuaqui <schuaquicano@gmail.com>
2021-07-26 18:46:13 -0400

    Ejercicios Consultas SQL V Parte 2

--- lend_gen_business_new.sql
+++ lend_gen_business_new.sql
@@ -30,8 +30,19 @@ FROM sites WHERE client_id=20
 GROUP BY client_id, month_created, year_created;
 
 --5--
-SELECT a.domain_name AS 'Nombre Dominio', count(leads_id) AS 'Contador'
+SELECT a.domain_name AS 'Nombre Dominio', count(leads_id) AS 'Contador' 
 FROM sites a, leads b WHERE a.site_id = b.site_id 
 AND b.registered_datetime BETWEEN "2011-1-1" AND "2011-2-15"
 GROUP BY a.domain_name;
 
+--6-- 
+SELECT CONCAT_WS(' ', clients.first_name, clients.last_name) AS 'Cliente', COUNT(leads.leads_id) AS 'Total' 
+FROM clients
+LEFT JOIN sites ON clients.client_id = sites.client_id
+LEFT JOIN leads ON sites.site_id = leads.site_id
+WHERE leads.registered_datetime >= STR_TO_DATE('1/1/2011', '%m/%d/%Y') 
+AND leads.registered_datetime <= STR_TO_DATE('12/31/2011', '%m/%d/%Y')
+GROUP BY CONCAT_WS(' ', clients.first_name, clients.last_name), clients.client_id
+ORDER BY clients.client_id;
+
+--7--
